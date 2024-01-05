d13dde9f5fb6441bf9891ca84c8e5c8a5e96ce9a
d68864767 <153361541+d68864767@users.noreply.github.com>
2024-01-04 21:21:02 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// scripts.js
+
+// This script file is currently empty as per the project description and the provided HTML and CSS files.
+// Depending on the future requirements, JavaScript code can be added here for interactivity, data fetching, form validation, etc.
